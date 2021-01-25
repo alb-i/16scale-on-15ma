d05e2b2a381839b59fac3ea95cd4668d558c2295
Immanuel Albrecht <mail@immanuel-albrecht.de>
2021-01-25 19:04:44 +0100

    ad-hoc codified my fretting preferences

--- tabs.py
+++ tabs.py
@@ -2,14 +2,16 @@
 
 import pitches as p
 
+from timeit import timeit
+
 unplayedString = "" # we use this to indicate that there is a string which is left alone :)
 
-tunings = {4:[-8   + i*5 for i in range(4)], #4-string bass
-           5:[-13  + i*5 for i in range(5)], #5-string bass
-           6:[-5   + i*5 for i in range(6)], # my 6-string tuning
-           7:[-5   + i*5 for i in range(7)], # my 7-string tuning
-           8:[-10  + i*5 for i in range(8)], # my 8-string tuning
-           'git':[4 + i for i in [0,5,10,15,19,24]] #standard guitar tuning
+tunings = {4:tuple([-8   + i*5 for i in range(4)]), #4-string bass
+           5:tuple([-13  + i*5 for i in range(5)]), #5-string bass
+           6:tuple([-5   + i*5 for i in range(6)]), # my 6-string tuning
+           7:tuple([-5   + i*5 for i in range(7)]), # my 7-string tuning
+           8:tuple([-10  + i*5 for i in range(8)]), # my 8-string tuning
+           'git':tuple([4 + i for i in [0,5,10,15,19,24]]) #standard guitar tuning
           }
           
 def transposeAndMap(listoflists, fn=lambda x: x,nothing=""):
@@ -95,13 +97,6 @@ def combineFrettings(frettings):
             current[i] = x
     return current
    
-def getChordFretCandidates(pitches, tuning=tunings[8],hifret=24):
-    candidates = [[]]
-    c0 = [getPitchFretCandidates(x) for x in pitches]
-    for c in c0:
-        candidates = [combineFrettings([x,y]) for x in candidates for y in c if canCombineFrettings([x,y])]
-    return candidates
-
 def getFretSpan(fretting):
     nbrs = [x for x in fretting if x != unplayedString and x != 0]
     if len(nbrs) == 0:
@@ -122,19 +117,38 @@ def getPlayedStrings(fretting):
     nbrs = [i for i,x in enumerate(fretting) if x != unplayedString]
     return nbrs
 
-openStringPenalty = 0
-neckMovePenalty = 100
+favoriteFretNbr = 5
+belowFavoriteFretPenalty = 6
+aboveFavoriteFretPenalty = 4
+openStringPenalty = 100 # 100 if you despice open string, 50 if you prefer no open strings; 15, 30, 20 also are reasonable numbers, even 0
+neckMovePenalty = 20
 stringChangePenalty = 75
 openMutePenalty = 250
-mutedSkippedStringPenalty = 30
+mutedSkippedStringPenalty = 100
 multipleSkippedStringsPenalty = 10 #for each additionally skipped string
 spanWidthPenalty = 10
 reverseFrettingPenalty = 40
 spanBiggerThan4Penalty = 120
 spanBiggerThan5Penalty = 1000
-fretHigherThan16Penalty = 50
 
-def scoreSuccessiveFrettings(prevfrets,currfrets):
+penalizeUpperFretsAfter = 13
+fretUpperFretsPenalty = 5
+
+
+penalizeFretsHigherThan = 16
+fretHigherThanXXPenalty = 50
+
+
+def scoreSuccessiveFrettings(prevfrets,currfrets,cache={}):
+    cacheEntry = (tuple(prevfrets),tuple(currfrets))
+    # this cache gives ~15x speedup
+    # >>> timeit(lambda: scoreSuccessiveFrettings([5,"",5],[6,8],cache={}),number=1000000)
+    # 11.593538855000006
+    # >>> timeit(lambda: scoreSuccessiveFrettings([5,"",5],[6,8]),number=1000000)
+    # 0.7480272119999967
+    if cacheEntry in cache:
+        return cache[cacheEntry]
+    
     penalty = 0
     # penalty for moving around on the neck
     prevSpan = getFretSpan(prevfrets)
@@ -169,14 +183,30 @@ def scoreSuccessiveFrettings(prevfrets,currfrets):
                     multilinegaps += 1
                 lastPlayed = False
         penalty += mutedSkippedStringPenalty*gaps + multipleSkippedStringsPenalty*multilinegaps
+        
+        
     
     fret_inversions = 0
-    for f0,f1 in zip(currfrets[:-1],currfrets[1:]):
-        if f0 == unplayedString or f1 == unplayedString:
-            continue
+    fretswithoutgaps = [x for x in currfrets if x != unplayedString]
+    for f0,f1 in zip(fretswithoutgaps[:-1],fretswithoutgaps[1:]):
+        #if f0 == unplayedString or f1 == unplayedString:
+        #    continue
         if f1 < f0:
             fret_inversions += 1
     penalty += fret_inversions * reverseFrettingPenalty
+    
+    below_favorite_fret_sum = 0
+    above_favorite_fret_sum = 0
+    for f in currfrets:
+        if f == unplayedString:
+            continue
+        if f < favoriteFretNbr and f != 0:
+            below_favorite_fret_sum += (favoriteFretNbr - f)
+        elif f > favoriteFretNbr:
+            above_favorite_fret_sum += (f - favoriteFretNbr)
+    
+    penalty += below_favorite_fret_sum*belowFavoriteFretPenalty
+    penalty += above_favorite_fret_sum*aboveFavoriteFretPenalty
     
     fretSpan = getFretSpan(currfrets)
     if fretSpan != None:
@@ -187,11 +217,102 @@ def scoreSuccessiveFrettings(prevfrets,currfrets):
         elif fs > 3:
             penalty += spanBiggerThan4Penalty
     
-    penalty += len([1 for x in currfrets if x != unplayedString and x > 16])*fretHigherThan16Penalty
+    penalty += len([1 for x in currfrets if x != unplayedString and x > penalizeFretsHigherThan])*fretHigherThanXXPenalty
+    penalty += len([1 for x in currfrets if x != unplayedString and x > penalizeUpperFretsAfter])*fretUpperFretsPenalty
     
     # penalty for the awkwardness of the current fretting
     penalty += openStringPenalty * len([1 for x in currfrets if x == 0])
+    
+    cache[cacheEntry] = penalty
     return penalty
 
 def scoreFretting(fretting):
     return scoreSuccessiveFrettings([],fretting)
+    
+def isImpossibleFretting(fretting):
+    fingered = [x for x in fretting if x != unplayedString and x > 0]
+    if not fingered:
+        return False
+    if max(fingered)-min(fingered) > 5: # +3 is normal, +4 works usually on guitars, +5 sometimes on guitars, +6 frets maybe at 24th fret 
+        return True
+    return False
+
+def getChordFretCandidates(pitches, tuning=tunings[8],hifret=24,cache={}):
+    """
+        Takes pitches and generates a list of possible frettings achieving these pitches.
+        The resulting list is sorted according to scoreFretting(..).
+    """
+    cacheEntry = (tuple(pitches),tuple(tuning),hifret) 
+    # this cache gives ~100x speedup
+    # >>> timeit.timeit(lambda: getChordFretCandidates([6,8]),number=10000)
+    # 0.009678113999996185
+    # >>> timeit.timeit(lambda: getChordFretCandidates([6,8],cache={}),number=10000)
+    # 1.1319672120000064
+    if cacheEntry in cache:
+        return cache[cacheEntry]
+    candidates = [[]]
+    c0 = [getPitchFretCandidates(x) for x in pitches]
+    for c in c0:
+        candidates = [combineFrettings([x,y]) for x in candidates 
+                                              for y in c if canCombineFrettings([x,y])]
+        candidates = [x for x in candidates if not isImpossibleFretting(x)]
+    # sort the candidates and combine the cache: our cache entries are 3-tuples, the scoreSuccessiveFrettings
+    # cache entries are 2-tuples.
+    candidates.sort(key=lambda x: scoreSuccessiveFrettings([],x,cache))
+    if candidates == []:
+        candidates = [[]] #regard unplayable stuff as a fancy rest
+    cache[cacheEntry] = candidates
+    return candidates
+
+def scoreFrettingSequence(seq, cache={}):
+    if len(seq) == 0:
+        return 0
+    scoresum = scoreSuccessiveFrettings([],seq[0],cache)
+    for p,c in zip(seq[:-1],seq[1:]):
+        scoresum += scoreSuccessiveFrettings(p,c,cache)
+    return scoresum
+
+def generateFrettingSequence(pitches, tuning=tunings[8], hifret=24, windowsize=5, cache={}):
+    """
+        takes a list of pitch-lists and turns them into a list of frettings
+        that optimizes its score. Dictionaries found in the pitch-lists
+        are considered to contain on-the-fly options.
+    """
+    # separate pitches from options
+    options = [[x for x in p if type(x) == dict] for p in pitches]
+    pitches = [[x for x in p if type(x) != dict] for p in pitches]
+    candidates = [getChordFretCandidates(p, tuning, hifret,cache) for p in pitches]
+    
+    def dfs(currentBest, currentBound, otherPrefix,prefixChoice, choiceCandidates):
+        if choiceCandidates == []:
+            return prefixChoice
+        for c in choiceCandidates[0]:
+            partialScore = scoreFrettingSequence(otherPrefix + prefixChoice+[c],cache)
+            if partialScore < currentBound:
+                nextBest = dfs(currentBest,currentBound,otherPrefix, prefixChoice+[c],choiceCandidates[1:])
+                if nextBest != currentBest:
+                    currentBest = nextBest
+                    currentBound = scoreFrettingSequence(otherPrefix + nextBest,cache)
+        return currentBest
+        
+    
+    bestcandidate = [c[0] for c in candidates]
+    
+    for i in range(len(pitches)):
+        opts = {}
+        for x in options[i]:
+            opts = dict(list(opts.items()) + list(x.items()))
+        if i > 0:
+            prev = bestcandidate[i-1]
+        else:
+            prev = []
+        N_i = min(i+windowsize,len(bestcandidate))
+        currentChunk = bestcandidate[i:N_i] # current best candidate solution
+        
+        scoreBound = scoreFrettingSequence([prev]+currentChunk,cache) # upper bound for best score
+        chunkCandidates = candidates[i:N_i]
+        bestChunk = dfs(currentChunk,scoreBound,[prev],[],chunkCandidates)
+        bestcandidate[i:N_i] = bestChunk
+        
+    
+    return bestcandidate
