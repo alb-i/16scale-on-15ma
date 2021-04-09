8b7f256599e31a60604314e58ad285429f952ae5
Immanuel Albrecht <mail@immanuel-albrecht.de>
2021-04-09 21:30:26 +0200

    renamed the pattern variable because hlint complains about it

--- h-16on15ma/src/Helpers.hs
+++ h-16on15ma/src/Helpers.hs
@@ -2,12 +2,12 @@ module Helpers where
 
 -- | checks whether the head of a list fits a given pattern
 testHeadPattern :: [a -> Bool] {-^ pattern tests -} -> [a] {-^ list to test -} -> Bool
-testHeadPattern pattern list | length pattern > length list = False
-                             | otherwise = all apply $ zip pattern list
+testHeadPattern ptest list | length ptest > length list = False
+                             | otherwise = all apply $ zip ptest list
                              where apply (f,x) = f x
 
 -- | detemine how often a specific pattern occurs in given list
 countPatternInList :: [a -> Bool] {-^ pattern tests -} -> [a] {-^ list to test -} -> Int
 countPatternInList _ [] = 0
-countPatternInList pattern list@(x:xs) | (testHeadPattern pattern list) = 1 + (countPatternInList pattern xs)
-                                       | otherwise                      = countPatternInList pattern xs
+countPatternInList ptest list@(x:xs) | testHeadPattern ptest list = 1 + countPatternInList ptest xs
+                                     | otherwise                      = countPatternInList ptest xs
